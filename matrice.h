7aa0cd7567b7168d0c0ec370e478b4fceb109208
alberto5941 <albertomaria.martellini@gmail.com>
2025-01-09 22:03:52 +0100

    Create matrice.h

---
+++ matrice.h
@@ -0,0 +1,44 @@
+#ifndef MATRICE_H_
+#define MATRICE_H_
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <stdexcept>
+#include "riga.h"
+using namespace std;
+
+class matrice {
+    private:
+    vector<riga> righe; //vettore di oggetti riga che rappresenta la matrice
+
+    public:
+    //costruttori
+    matrice(size_t numero_righe, size_t numero_colonne); //matrice vuota
+    matrice(vector<vector<double> > valori); //matrice con valori iniziali
+
+    //metodi
+    void set_riga(size_t index, const riga& nuova_riga); //metodo per settare il contenuto di una riga della matrice
+    void set_elemento(size_t index_riga, size_t index_colonna, double value);
+    void stampa(); // metodo per stampare la matrice su schermo
+    size_t get_numero_righe() const;
+    size_t get_numero_colonne() const;
+    double get(size_t riga, size_t colonna) const;
+    matrice calcola_trasposta();
+    double calcola_traccia();
+    bool verifica_simmetria();
+    bool verifica_antisimmetria();
+
+
+    //operatori
+    matrice operator+(const matrice& other) const;
+    matrice operator*(double scalar) const;
+    friend matrice operator*(double scalar, const matrice& M);
+    matrice operator-(const matrice& other) const;
+    matrice operator*(const matrice& other) const;
+};
+
+
+#endif // MATRICE_H_
