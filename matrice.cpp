526004c5f17e71a11e50e2b9e8f91008dff60bb2
alberto5941 <albertomaria.martellini@gmail.com>
2025-01-10 13:52:54 +0100

    Update matrice.cpp

--- matrice.cpp
+++ matrice.cpp
@@ -19,6 +19,12 @@ matrice::matrice(const vector<vector<double> > valori) {
     }
 }
 
+matrice::matrice(const vector<riga> vec_righe) {
+    for(int i=0; i<vec_righe.size(); i++) {
+        righe.push_back(vec_righe.at(i));
+    }
+}
+
 void matrice::set_riga(size_t index, const riga& nuova_riga) {
     if(index >= 0 && index <righe.size()) {
         righe[index] = nuova_riga;
@@ -115,21 +121,18 @@ void matrice::set_elemento(size_t index_riga, size_t index_colonna, double value
     }
 }
 
-matrice matrice::calcola_trasposta() {
+matrice matrice::calcola_trasposta_copia() {
     size_t n = get_numero_righe();
-    vector<double> riga;
-    matrice trasposta(n, n);
-    if(get_numero_righe() != get_numero_colonne()) {
-        throw invalid_argument("la matrice deve essere quadrata per poterne calcolare la trasposta");
-    } else {
-        for(size_t i=0; i<n; i++) {
-            for(size_t j=0; j<n; j++) {
-                double value = get(i, j);
-                trasposta.set_elemento(j, i, value);
-            }
+    vector<riga> righe_trasposte;
+    riga riga_trasposta(n);
+    for(size_t i=0; i<get_numero_colonne(); i++) {
+        for(size_t j=0; j<n; j++) {
+            riga_trasposta.set_valore(j, righe.at(j).get_valore(i));
         }
+        righe_trasposte.push_back(riga_trasposta);
     }
-    return trasposta;
+    matrice copia(righe_trasposte);
+    return copia;
 }
 
 double matrice::calcola_traccia() {
@@ -189,4 +192,97 @@ matrice matrice::operator*(const matrice& other) const {
         }
     }
     return matrice(valori_risultato);
+}
+
+matrice matrice::elimina_riga_copia(size_t index) {
+    if(index < 0 || index>righe.size()) {
+        throw out_of_range("indice fuori dai limiti: puoi eliminare solo una riga che appartiene alla matrice ");
+    }
+    matrice copia = *this;
+    copia.righe.erase(copia.righe.begin() + index);
+    return copia;
+}
+
+matrice matrice::elimina_colonna_copia(size_t index) {
+    if(index < 0 || index>righe.size()) {
+        throw out_of_range("indice fuori dai limiti: puoi eliminare solo una riga che appartiene alla matrice ");
+    }
+    matrice trasposta_copia = calcola_trasposta_copia();
+    matrice senza_riga = trasposta_copia.elimina_riga_copia(index);
+    matrice senza_colonna(get_numero_righe(), get_numero_colonne()-1);
+    senza_colonna = senza_riga.calcola_trasposta_copia();
+    return senza_colonna;
+}
+
+matrice matrice::elimina_riga_colonna_copia(size_t r, size_t c) {
+    if(r < 0 || r>righe.size() || c<0 || c>get_numero_colonne()) {
+        throw out_of_range("indice fuori dai limiti: puoi eliminare solo una riga che appartiene alla matrice ");
+    }
+    matrice nuova1 = *this;
+    matrice nuova2 = *this;
+    nuova1 = elimina_riga_copia(r);
+    nuova2 = nuova1.elimina_colonna_copia(c);
+
+    return nuova2;
+}
+
+double matrice::get_determinante() {
+    if(get_numero_righe() != get_numero_colonne()) {
+        throw invalid_argument("puoi calcolare il determinante solo di una matrice quadrata");
+    }
+    double determinante;
+    if(get_numero_righe() == 1) {
+        determinante = get(0,0);
+    }
+    if(get_numero_righe() == 2) {
+        determinante = get(0, 0)*get(1, 1) - get(0, 1)*get(1, 0);
+    }
+    if(get_numero_righe() > 2) {
+        double sommatoria = 0;
+        for(size_t i=0; i<get_numero_righe(); i++) {
+                matrice tagliata = elimina_riga_colonna_copia(i, 0);
+                double elemento = tagliata.get_determinante()*get(i, 0);
+                sommatoria = sommatoria + elemento*pow(-1, i);
+            }
+        determinante = sommatoria;
+    }
+    return determinante;
+}
+
+bool matrice::verifica_invertibilita() {
+    bool invertibile;
+    if(get_numero_righe() != get_numero_colonne()) {
+        throw invalid_argument("non esiste inversa: la matrice deve essere quadrata");
+    }
+    if(get_determinante() == 0) {
+        invertibile = false;
+    } else {
+        invertibile = true;
+    }
+    return invertibile;
+}
+
+matrice matrice::get_inversa() {
+    if(get_numero_righe() != get_numero_colonne()) {
+        throw invalid_argument("non esiste inversa: la matrice deve essere quadrata");
+    }
+    bool invertibile = verifica_invertibilita();
+    if(invertibile==false) {
+        throw invalid_argument("la matrice non ammette inversa");
+    }
+    vector<riga> righe_cofattori;
+    size_t n = get_numero_righe();
+    riga riga_cofattori(n);
+    for(size_t i=0; i<n; i++) {
+        for(size_t j=0; j<n; j++) {
+            matrice tagliata = elimina_riga_colonna_copia(i, j);
+            double value = tagliata.get_determinante();
+            riga_cofattori.set_valore(j, value);
+        }
+        righe_cofattori.push_back(riga_cofattori);
+    }
+    matrice cofattori(righe_cofattori);
+    matrice inversa(n, n);
+    inversa = (1/get_determinante())*cofattori.calcola_trasposta_copia();
+    return inversa;
 }
